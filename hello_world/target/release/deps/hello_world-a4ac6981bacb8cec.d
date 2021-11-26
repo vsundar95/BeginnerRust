bcde9a24aae3637ae8eeef3d8d0d13eb4b7aeb0e
VinnySundar <vinnysundar@pop-os.localdomain>
2021-11-25 21:07:09 -0500

    Created a release directory

---
+++ hello_world/target/release/deps/hello_world-a4ac6981bacb8cec.d
@@ -0,0 +1,5 @@
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/release/deps/hello_world-a4ac6981bacb8cec: src/main.rs
+
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/release/deps/hello_world-a4ac6981bacb8cec.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/release/hello_world: /home/vinnysundar/Documents/git/BeginnerRust/hello_world/src/main.rs
