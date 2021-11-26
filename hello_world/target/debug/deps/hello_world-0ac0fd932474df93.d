4d0234915bac080593d2de7a8c66421e2a0321f9
VinnySundar <vinnysundar@pop-os.localdomain>
2021-11-25 21:20:37 -0500

    Created a debug test directory for any unit tests

---
+++ hello_world/target/debug/deps/hello_world-0ac0fd932474df93.d
@@ -0,0 +1,5 @@
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/debug/deps/hello_world-0ac0fd932474df93: src/main.rs
+
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/debug/deps/hello_world-0ac0fd932474df93.d: src/main.rs
+
+src/main.rs:
