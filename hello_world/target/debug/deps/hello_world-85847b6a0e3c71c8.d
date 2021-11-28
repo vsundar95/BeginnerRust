0c72a8dbce81b0e23e68f476457f07d53d51cc2b
VinnySundar <vinnysundar@pop-os.localdomain>
2021-11-27 20:42:32 -0500

    target/debug changes

---
+++ hello_world/target/debug/deps/hello_world-85847b6a0e3c71c8.d
@@ -0,0 +1,5 @@
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/debug/deps/hello_world-85847b6a0e3c71c8.rmeta: src/main.rs
+
+/home/vinnysundar/Documents/git/BeginnerRust/hello_world/target/debug/deps/hello_world-85847b6a0e3c71c8.d: src/main.rs
+
+src/main.rs:
---
+++ l4-variables/target/debug/deps/l4_variables-5b993574bc13f337.d
@@ -0,0 +1,5 @@
+/home/vinnysundar/Documents/git/BeginnerRust/l4-variables/target/debug/deps/l4_variables-5b993574bc13f337.rmeta: src/main.rs
+
+/home/vinnysundar/Documents/git/BeginnerRust/l4-variables/target/debug/deps/l4_variables-5b993574bc13f337.d: src/main.rs
+
+src/main.rs:
